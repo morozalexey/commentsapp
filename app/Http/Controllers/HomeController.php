e182e257209045e88b5c5856dd8962224cbabf18
morozalexey <alexey.moroz87@gmail.com>
2020-01-27 06:59:37 +0300

    work with model

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Comment;
+
 class HomeController extends Controller
 {
     /**
@@ -21,8 +23,11 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
-        return view('home');
+    public function index(Request $request)
+    {   
+        $comments = Comment::where('hide', 0)->get();
+        return view('welcome', ['comments' => $comments]);
     }
+
+    
 }
